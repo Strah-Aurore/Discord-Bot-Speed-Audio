533bd8769d46e34fb5505f63136dfb49e05c8c6e
Strah <sheeped42@gmail.com>
2024-08-18 10:24:52 +0200

    Ajustments

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def speedup_message(message, speed):
 	
 	# Convert original .ogg file into a .wav file
 	x = pydub.AudioSegment.from_file(voice_file)
-	final = speedup(x, playback_speed=(speed))
+	final = speedup(x, speed,150,25)
 	buffer = io.BytesIO()
 	timenow = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
 	#final.export("C:/Users/Aurore/Documents/project/Discord-Bot-Speed-Audio/temp/"+ timenow +".mp3",format="mp3")
@@ -59,18 +59,18 @@ async def speedup_message(message, speed):
 
 
 	
-@bot.message_command(name="accelere 1.25", force_global=True)
-async def accelere25(interaction: nextcord.Interaction, message: nextcord.Message):
+@bot.message_command(name="speed 1.25", force_global=True)
+async def speed25(interaction: nextcord.Interaction, message: nextcord.Message):
 	await interaction.response.send_message(content="Arriba ! Arriba ! Je te prépare ça dès que possible !", ephemeral=True)
 	await speedup_message(message, 1.25)
 	
-@bot.message_command(name="accelere 1.5", force_global=True)
-async def accelere50(interaction: nextcord.Interaction, message: nextcord.Message):
+@bot.message_command(name="speed 1.50", force_global=True)
+async def speed50(interaction: nextcord.Interaction, message: nextcord.Message):
 	await interaction.response.send_message(content="Arriba ! Arriba ! Je te prépare ça dès que possible !", ephemeral=True)
 	await speedup_message(message, 1.5)
 
-@bot.message_command(name="accelere 1.75", force_global=True)
-async def accelere75(interaction: nextcord.Interaction, message: nextcord.Message):
+@bot.message_command(name="speed 1.75", force_global=True)
+async def speed75(interaction: nextcord.Interaction, message: nextcord.Message):
 	await interaction.response.send_message(content="Arriba ! Arriba ! Je te prépare ça dès que possible !", ephemeral=True)
 	await speedup_message(message, 1.75)
 	
